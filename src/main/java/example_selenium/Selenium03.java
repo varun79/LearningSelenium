a9e72185e0602a3a13494e9825001fbfddef7cd4
varun79 <vpsonavane@gmail.com>
2024-10-18 19:19:38 +0530

    files modified

--- src/main/java/example_selenium/Selenium03.java
+++ src/main/java/example_selenium/Selenium03.java
@@ -2,9 +2,14 @@ package example_selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
 
 public class Selenium03 {
-    public static void main(String[] args) {
+
+    @Test
+    public void test01()
+     {
 
         WebDriver driver = new EdgeDriver();
         driver.get("https://sdet.live");
--- src/main/java/example_selenium/Selenium07.java
+++ src/main/java/example_selenium/Selenium07.java
@@ -1,19 +1,47 @@
 package example_selenium;
 
-import org.junit.Assert;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 
 public class Selenium07 {
-    public static void main(String[] args) {
 
+   @Test
+   public void testGetTitle()
+    {
         WebDriver driver= new EdgeDriver();
         driver.manage().window().maximize();
         driver.get("https://katalon-demo-cura.herokuapp.com/");
 
         Assert.assertEquals(driver.getTitle(),"CURA Healthcare Service");
+        driver.close();
+
+    }
+
+    @Test
+    public void testGetCurrentURL()
+    {
+        WebDriver driver= new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://katalon-demo-cura.herokuapp.com/");
+
         Assert.assertEquals(driver.getCurrentUrl(),"https://katalon-demo-cura.herokuapp.com/");
+        driver.close();
+    }
+
+    @Test
+    public void testPageSource()
+    {
+        WebDriver driver= new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://katalon-demo-cura.herokuapp.com/");
 
         Assert.assertTrue(driver.getPageSource().contains("CURA Healthcare Service"));
+        driver.close();
+
     }
+
 }
